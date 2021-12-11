5f287ea448b9cbd3a87945253332185a40beb4ab
AlexandraSharipova <79837258+AlexandraSharipova@users.noreply.github.com>
2021-12-11 21:26:45 +0400

    Add files via upload

--- image_manipulations/adjust_hsv.py
+++ image_manipulations/adjust_hsv.py
@@ -3,12 +3,14 @@ from utils import *
 
 def adjust_hsv(state, block):
     DEFAULT_HUE = .5
-
+    DEFAULT_Value = .5
+    DEFAULT_Saturation = .5
     hsv_img = rgb2hsv(state['image'])
 
     h = block.slider('Hue', min_value=0., max_value=1., value=DEFAULT_HUE)
     hsv_img[:, :, 0] += (h - DEFAULT_HUE) % 1.
-
-    # TODO: adjust saturation and brightness
-
+    v = block.slider('VAL', min_value=0., max_value=1., value=DEFAULT_HUE)
+    hsv_img[:, 0, :] += (v - DEFAULT_Value) % 1.
+    s = block.slider('SAT', min_value=0., max_value=1., value=DEFAULT_HUE)
+    hsv_img[0, :, :] += (s - DEFAULT_Saturation) % 1.
     state['image'] = hsv2rgb(hsv_img)
